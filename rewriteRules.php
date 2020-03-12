c3f6600f9c02a2086ea52180f82c73d8fe63db9f
toto <mihai.totoianu@gmail.com>
2020-03-12 15:54:41 +0200

    new files
    added new files

--- rewriteRules.php
+++ rewriteRules.php
@@ -2,8 +2,10 @@
 
 // Available placeholders: :uc:vendor, :uc:package, :lc:vendor, :lc:package
 return [
-    'src/MyPackage.php' => 'src/:uc:package.php',
+    
     'config/mypackage.php' => 'config/:lc:package.php',
     'src/Facades/MyPackage.php' => 'src/Facades/:uc:package.php',
+    'src/Models/MyPackage.php' => 'src/Models/:uc:package.php',
     'src/MyPackageServiceProvider.php' => 'src/:uc:packageServiceProvider.php',
+    'src/Controllers/MyPackageController.php' => 'src/Controllers/:uc:packageController.php',
 ];
---
+++ src/Controllers/MyPackageController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace :uc:vendor\:uc:package;
+
+use App\Http\Controllers\Controller;
+
+class :uc:packageController extends Controller
+{
+	
+}
