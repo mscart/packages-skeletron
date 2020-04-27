c338be6b56f42a0f3764283cbe53146bab283d29
MsCart <totolino_1979@yahoo.com>
2020-04-27 16:57:21 +0300

    modified skeleton add menu

--- src/MyPackageServiceProvider.php
+++ src/MyPackageServiceProvider.php
@@ -35,7 +35,7 @@ class :uc:packageServiceProvider extends ServiceProvider
         
         $menu =  \Menu::get('Sidebar');
 		$acl = $menu->add(__(':lc:package:::sglc:package.name'),    ['segment2'=>':lc:package', 'icon'=> 'icon-cart'])->nickname(':sglc:package')->data('order', 1);
-		$menu->order->add(__(':lc:package:::sglc:package.list'),config('app.admin_prefix').'/:lc:package');
+		$menu->:sglc:package->add(__(':lc:package:::sglc:package.list'),config('app.admin_prefix').'/:lc:package');
     }
 
     /**
